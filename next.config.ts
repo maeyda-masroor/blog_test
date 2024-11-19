7d7df258749b40367b1677a812a2c5d155c6a142
maeyda-masroor <78406096+maeyda-masroor@users.noreply.github.com>
2024-11-19 15:59:03 +0500

    add file check changes
    cc

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tushi-nextjs.vercel.app',
+      },
+      {
+        protocol:'https',
+        hostname:'via.placeholder.com'
+      }
+    ],
+  }
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Header from '../components/Header';
+import Blog from '../components/Blog1';
+export default function Home(){
+    return <div>
+        <Header/>
+        <Blog/>
+    </div>
+}
